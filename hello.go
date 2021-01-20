a819e85cfeba28366223039a3fc1d770d78ef8e7
Daniel Hoyas <hmartindaniel@gmail.com>
2021-01-20 16:56:42 +0100

    test

--- hello.go
+++ hello.go
@@ -16,11 +16,4 @@ func main() {
 
 	log.Fatal(app.Listen(":3000"))
 
-	// // GET /john
-	// app.Get("/:name", func(c *fiber.Ctx) error {
-	// 	msg := fmt.Sprintf("Hello, %s 👋!", c.Params("name"))
-	// 	return c.SendString(msg) // => Hello john 👋!
-	// })
-
-	// log.Fatal(app.Listen(":3000"))
 }
